30acb7421b41847d1070ec84c4757a0fbec4d9db
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-06-16 00:19:30 +0100

    Trace try statement (#39)
    * Parse try statement
    
    * Fix?
    
    * Fix syntax

--- src/testcases/syntax/example1.sol
+++ src/testcases/syntax/example1.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
+contract Parent {
+    constructor() {
+    }
+
+    function call() external {
+    }
+}
+
 contract Example {
     uint256 value;
 
@@ -32,5 +40,18 @@ contract Example {
 
     function simple_with_modifier() public simpleModifier {
         value = 5;
+    }
+
+    function with_try_statement() public {
+        try new Parent() returns (Parent newContract) {
+            value = 1;
+        } catch Error(string memory reason) {
+            value = 2;
+        } catch Panic(uint errorCode) {
+            value = 3;
+        } catch (bytes memory lowLevelData) {
+            value = 4;
+        }
+
     }
 }
