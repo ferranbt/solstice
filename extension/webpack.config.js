749b56360ee564c085a576bf46fa82f1c7d0c9ec
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-07-22 11:07:04 +0100

    Put extension js code in its own separated folder

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -8,7 +8,7 @@ const path = require("path");
 const config = {
 	target: "webworker", // vscode extensions run in webworker context for VS Code web 📖 -> https://webpack.js.org/configuration/target/#target
 
-	entry: "./client/src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+	entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
 	output: {
 		// the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
 		path: path.resolve(__dirname, "dist"),
