2e94979eefc05072502b29c7ef303d45ab3d6f61
Ferran Borreguero <ferran.borreguero@gmail.com>
2025-07-31 16:10:51 +0100

    Format

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -186,6 +186,7 @@ async function startLanguageClient(context: ExtensionContext, config: Config) {
     middleware: createMiddleware(),
     initializationOptions: config.cfg,
     initializationFailedHandler: (error) => {
+      log.error("Initialization failed:", error);
       return false;
     },
     synchronize: {
@@ -237,7 +238,8 @@ export function deactivate(): Thenable<void> | undefined {
 }
 
 class MockDebugAdapterNamedPipeServerDescriptorFactory
-  implements vscode.DebugAdapterDescriptorFactory {
+  implements vscode.DebugAdapterDescriptorFactory
+{
   private _server?: Net.Server;
 
   createDebugAdapterDescriptor(
